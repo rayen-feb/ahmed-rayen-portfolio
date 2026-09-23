4fce8cb4584381f469d365584557ab72d4cd8c61
rayen-feb <rayen.bouazizi@esprit.tn>
2026-09-23 14:46:36 +0100

    Publish Ahmed Rayen portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rayen-feb.github.io",
+  base: "/cv-site",
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
